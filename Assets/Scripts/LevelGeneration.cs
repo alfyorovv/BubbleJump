33818c1125bff2b4e70b651a4620ae7a694e8633
alfyorovv <73781174+alfyorovv@users.noreply.github.com>
2022-08-07 16:44:07 +0200

    Spikes random generation

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,23 +6,42 @@ public class LevelGeneration : MonoBehaviour
 {
     public GameObject walls;
     public GameObject spawnPoint;
+    public GameObject spike;
+    GameObject[] obstacleSpawnPoints;
+
     Vector2 spawnPosition;
+    int rand;
 
     private void Start()
     {
         spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
         spawnPosition = spawnPoint.transform.position;
+        obstacleSpawnPoints = GameObject.FindGameObjectsWithTag("ObstacleSpawnPoint");
+
+        foreach (GameObject obj in obstacleSpawnPoints)
+        {
+           rand = Random.Range(0, 10);
+           if (rand<3)
+           {
+               Instantiate(spike, obj.transform.position, Quaternion.identity, walls.transform);
+           }
+        }
     }
 
     private void Update()
     {
         if (transform.position.y < -10)
+        {
             Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Instantiate(walls, spawnPosition, Quaternion.identity);
+        if (collider.gameObject.tag == "SpawnTrigger")
+        {
+           Instantiate(walls, spawnPosition, Quaternion.identity);
+        }
     }
 
 }
